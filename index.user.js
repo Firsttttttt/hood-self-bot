ea9248f424e3c974ce92d9df29ac7e553d05cf49
Hoodgail <45743294+Hoodgail@users.noreply.github.com>
2020-08-21 11:06:29 -0400

    Create index.user.js

---
+++ index.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        Hood's selfbot
+// @namespace   Hoodgail scripts
+// @match       *://discord.com/*
+// @website     https://github.com/hoodJS/hood-self-bot/
+// @grant       none
+// @version     1.0
+// @author      Hoodgail
+// @description a selfbot for discord to do cool stuff
+// @icon        https://cdn.discordapp.com/attachments/713693326884339723/746382349880655892/image_2.png
+// ==/UserScript==
